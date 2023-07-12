8b7d19ccc6c38f093b5ee909083b97ea67ba617a
IgorWestermann <i.westma@gmail.com>
2023-07-12 05:05:27 -0300

    hotfix: ajustando layout

--- src/Mocks/friends.js
+++ src/Mocks/friends.js
@@ -1,102 +1,102 @@
 export const friends = [
-  {
-    id: "1",
-    fullName: "Naruto",
-    avatarUrl: require("../../assets/fotos-perfil/naruto.jpg"),
-    description:
-      "Eu sou um ninja barulhento, desastrado e imprevisível. Tenho o sonho de me tornar o Hokage, o líder da minha vila. Minha determinação e força inabaláveis me levam a enfrentar qualquer desafio que apareça no meu caminho.",
-    commonFriends: ["3", "6", "8"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "2",
-    fullName: "Gaara",
-    avatarUrl: require("../../assets/fotos-perfil/gaara.jpeg"),
-    description:
-      "Eu sou um ninja com a habilidade de controlar e manipular a areia. Meu objetivo é proteger minha vila e meus entes queridos, mesmo que isso signifique lutar contra poderosos inimigos. Sou conhecido por ser um dos Shinobis mais poderosos de todos os tempos.",
-    commonFriends: ["1", "4", "9"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "3",
-    fullName: "Hinata",
-    avatarUrl: require("../../assets/fotos-perfil/hinata.jpeg"),
-    description:
-      "Eu sou uma kunoichi tímida e gentil, mas extremamente determinada. Treinei arduamente para me tornar uma ninja habilidosa e sempre busco superar meus limites. Meu amor e lealdade pelo Naruto são inabaláveis.",
-    commonFriends: ["6", "7", "10"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "4",
-    fullName: "Ino",
-    avatarUrl: require("../../assets/fotos-perfil/ino.jpeg"),
-    description:
-      "Eu sou uma ninja especializada em técnicas de controle mental e sensoriais. Com minhas habilidades, posso ler os pensamentos dos outros e comunicar-me com meus aliados a longas distâncias. Estou sempre pronta para ajudar meus amigos em batalhas e na vida cotidiana.",
-    commonFriends: ["2", "5", "9"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "5",
-    fullName: "Rock Lee",
-    avatarUrl: require("../../assets/fotos-perfil/rocklee.jpg"),
-    description:
-      "Eu sou um ninja que se especializa em taijutsu, o combate corpo a corpo. Mesmo sem habilidades de ninjutsu ou genjutsu, treino arduamente para me tornar um dos maiores guerreiros da vila. Minha dedicação e determinação são minhas maiores armas.",
-    commonFriends: ["1", "4", "7"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "6",
-    fullName: "Sasuke",
-    avatarUrl: require("../../assets/fotos-perfil/sasuke.jpeg"),
-    description:
-      "Eu sou um ninja que busca vingança e poder para me tornar mais forte. Meu objetivo é vingar o clã Uchiha e proteger minha vila a todo custo. Minhas habilidades de ninjutsu e genjutsu são consideradas uma das mais poderosas.",
-    commonFriends: ["3", "8", "10"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "7",
-    fullName: "Shikamaru",
-    avatarUrl: require("../../assets/fotos-perfil/shikamaru.jpg"),
-    description:
-      "Eu sou um ninja inteligente e estrategista. Minhas habilidades de análise e planejamento são altamente valorizadas em batalhas e missões. Apesar de minha aparência preguiçosa, estou sempre um passo à frente dos meus inimigos.",
-    commonFriends: ["3", "5", "9"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "8",
-    fullName: "Itachi",
-    avatarUrl: require("../../assets/fotos-perfil/itachi.jpg"),
-    description:
-      "Eu sou um ninja poderoso e misterioso do clã Uchiha. Minhas habilidades de genjutsu são incomparáveis e carrego um grande fardo por minhas escolhas no passado. Meu amor pelo meu irmão é incondicional e estou sempre disposto a protegê-lo.",
-    commonFriends: ["1", "6", "10"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "9",
-    fullName: "Pain",
-    avatarUrl: require("../../assets/fotos-perfil/pain.jpeg"),
-    description:
-      "Eu sou um ninja líder da organização Akatsuki. Tenho o objetivo de criar paz através da dor e sofrimento. Minhas habilidades únicas e meu visual intimidador me tornam um dos inimigos mais temidos pelos ninjas de Konoha.",
-    commonFriends: ["2", "4", "7"],
-    comment: "Ótimo papo!",
-  },
-  {
-    id: "10",
-    fullName: "Deidara",
-    avatarUrl: require("../../assets/fotos-perfil/deidara.png"),
-    description:
-      "Eu sou uma pessoa apaixonada por arte explosiva e adoro criar esculturas usando argila explosiva. Minhas obras são verdadeiras explosões de criatividade e busco sempre desafiar os limites da arte com minhas habilidades únicas.",
-    commonFriends: ["2", "5", "8"],
-    comment: "Ótimo papo!",
-  },
+    {
+        id: "1",
+        fullName: "Naruto",
+        avatarUrl: require("../../assets/fotos-perfil/naruto.jpg"),
+        description:
+            "Tenho o sonho de me tornar o Hokage, o líder da minha vila. Minha determinação e força inabaláveis me levam a enfrentar qualquer desafio que apareça no meu caminho.",
+        commonFriends: ["3", "6", "8"],
+        comment: "Caronas sempre animadas!",
+    },
+    {
+        id: "2",
+        fullName: "Gaara",
+        avatarUrl: require("../../assets/fotos-perfil/gaara.jpeg"),
+        description:
+            "Eu sou um ninja com a habilidade de controlar e manipular a areia. Meu objetivo é proteger minha vila e meus entes queridos, mesmo que isso signifique lutar contra poderosos inimigos. Sou conhecido por ser um dos Shinobis mais poderosos de todos os tempos.",
+        commonFriends: ["1", "4", "9"],
+        comment: "Caronas confiáveis!",
+    },
+    {
+        id: "3",
+        fullName: "Hinata",
+        avatarUrl: require("../../assets/fotos-perfil/hinata.jpeg"),
+        description:
+            "Eu sou uma kunoichi tímida e gentil, mas extremamente determinada. Treinei arduamente para me tornar uma ninja habilidosa e sempre busco superar meus limites. Meu amor e lealdade pelo Naruto são inabaláveis.",
+        commonFriends: ["6", "7", "10"],
+        comment: "Viagens confortáveis!",
+    },
+    {
+        id: "4",
+        fullName: "Ino",
+        avatarUrl: require("../../assets/fotos-perfil/ino.jpeg"),
+        description:
+            "Eu sou uma ninja especializada em técnicas de controle mental e sensoriais. Estou sempre pronta para ajudar meus amigos em batalhas e na vida cotidiana.",
+        commonFriends: ["2", "5", "9"],
+        comment: "Caronas úteis!",
+    },
+    {
+        id: "5",
+        fullName: "Rock Lee",
+        avatarUrl: require("../../assets/fotos-perfil/rocklee.jpg"),
+        description:
+            "Eu sou um ninja especializado em taijutsu, o combate corpo a corpo. Minha dedicação e determinação são minhas maiores armas.",
+        commonFriends: ["1", "4", "7"],
+        comment: "Caronas cheias de energia!",
+    },
+    {
+        id: "6",
+        fullName: "Sasuke",
+        avatarUrl: require("../../assets/fotos-perfil/sasuke.jpeg"),
+        description:
+            "Eu sou um ninja que busca vingança e poder para me tornar mais forte. Meu objetivo é vingar o clã Uchiha e proteger minha vila a todo custo. Minhas habilidades de ninjutsu e genjutsu são consideradas uma das mais poderosas.",
+        commonFriends: ["3", "8", "10"],
+        comment: "Viagens emocionantes!",
+    },
+    {
+        id: "7",
+        fullName: "Shikamaru",
+        avatarUrl: require("../../assets/fotos-perfil/shikamaru.jpg"),
+        description:
+            "Eu sou um ninja inteligente e estrategista. Minhas habilidades de análise e planejamento são altamente valorizadas em batalhas e missões. Apesar de minha aparência preguiçosa, estou sempre um passo à frente dos meus inimigos.",
+        commonFriends: ["3", "5", "9"],
+        comment: "Caronas bem planejadas!",
+    },
+    {
+        id: "8",
+        fullName: "Itachi",
+        avatarUrl: require("../../assets/fotos-perfil/itachi.jpg"),
+        description:
+            "Eu sou um ninja poderoso e misterioso do clã Uchiha. Minhas habilidades de genjutsu são incomparáveis e carrego um grande fardo por minhas escolhas no passado. Meu amor pelo meu irmão é incondicional e estou sempre disposto a protegê-lo.",
+        commonFriends: ["1", "6", "10"],
+        comment: "Caronas confiáveis!",
+    },
+    {
+        id: "9",
+        fullName: "Pain",
+        avatarUrl: require("../../assets/fotos-perfil/pain.jpeg"),
+        description:
+            "Eu sou um ninja líder da organização Akatsuki. Tenho o objetivo de criar paz através da dor e sofrimento. Minhas habilidades únicas e meu visual intimidador me tornam um dos inimigos mais temidos pelos ninjas de Konoha.",
+        commonFriends: ["2", "4", "7"],
+        comment: "Viagens emocionantes!",
+    },
+    {
+        id: "10",
+        fullName: "Deidara",
+        avatarUrl: require("../../assets/fotos-perfil/deidara.png"),
+        description:
+            "Eu sou uma pessoa apaixonada por arte explosiva e adoro criar esculturas usando argila explosiva. Minhas obras são verdadeiras explosões de criatividade e busco sempre desafiar os limites da arte com minhas habilidades únicas.",
+        commonFriends: ["2", "5", "8"],
+        comment: "Caronas cheias de surpresas!",
+    },
 ];
 
 export const myProfile = {
-  id: "0",
-  fullName: "Luffy",
-  avatarUrl: require("../../assets/fotos-perfil/luffy.jpg"),
-  description:
-    "Eu sou o capitão dos Piratas do Chapéu de Palha e estou em busca do maior tesouro do mundo, o One Piece. Com o poder da Gomu Gomu no Mi, sou capaz de esticar meu corpo como borracha e tenho habilidades de combate excepcionais. Aventureiro, valente e sempre com um sorriso no rosto, estou determinado a realizar meus sonhos e proteger meus amigos",
-  commonFriends: ["1"],
-  comment: "Ótimo papo!",
+    id: "0",
+    fullName: "Luffy",
+    avatarUrl: require("../../assets/fotos-perfil/luffy.jpg"),
+    description:
+        "Eu sou o capitão dos Piratas do Chapéu de Palha e estou em busca do maior tesouro do mundo, o One Piece. Com o poder da Gomu Gomu no Mi, sou capaz de esticar meu corpo como borracha e tenho habilidades de combate excepcionais. Aventureiro, valente e sempre com um sorriso no rosto, estou determinado a realizar meus sonhos e proteger meus amigos",
+    commonFriends: ["1"],
+    comment: "Ótimo papo!",
 };
