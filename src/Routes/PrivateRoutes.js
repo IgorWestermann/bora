b93511e54f48c878ca80121dcf7b5f2fa90a7bcb
IgorWestermann <i.westma@gmail.com>
2023-07-12 02:07:45 -0300

    bugfix: ajustando performance

--- src/Routes/PrivateRoutes.js
+++ src/Routes/PrivateRoutes.js
@@ -7,68 +7,70 @@ import Search from "../screens/private/Search";
 import FriendsStack from "./Stacks/FriendsStack";
 import Profile from "../screens/private/Profile";
 import SearchStack from "./Stacks/SearchStack";
+import { useTheme } from "native-base";
 
 const Tab = createBottomTabNavigator();
 
 export default () => {
-  return (
-    <Tab.Navigator
-      tabBarStyle={{
-        backgroundColor: "#8b5cf6",
-        borderTopColor: "transparent",
-        flex: 1,
-      }}
-      screenOptions={{
-        tabBarActiveTintColor: "#7c3aed",
-        tabBarActiveBackgroundColor: "#c4b5fd",
-        tabBarInactiveTintColor: "#8b5cf6",
-        tabBarInactiveBackgroundColor: "#f5f3ff",
-      }}
-    >
-      <Tab.Screen
-        name="SearchStack"
-        component={SearchStack}
-        options={{
-          headerShown: false,
-          tabBarLabel: "Inicio",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="RidesStack"
-        component={RideStack}
-        options={{
-          headerShown: false,
-          tabBarLabel: "Corridas",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="car" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="FriendsStack"
-        component={FriendsStack}
-        options={{
-          headerShown: false,
-          tabBarLabel: "Amigos",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="users" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          headerShown: false,
-          tabBarLabel: "Perfil",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="user-circle" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
+    const theme = useTheme();
+    return (
+        <Tab.Navigator
+            tabBarStyle={{
+                backgroundColor: theme.colors.primary[500],
+                borderTopColor: "transparent",
+                flex: 1,
+            }}
+            screenOptions={{
+                tabBarActiveTintColor: theme.colors.primary[500],
+                tabBarActiveBackgroundColor: theme.colors.primary[100],
+                tabBarInactiveTintColor: theme.colors.primary[500],
+                tabBarInactiveBackgroundColor: theme.colors.primary[0],
+            }}
+        >
+            <Tab.Screen
+                name="SearchStack"
+                component={SearchStack}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: "Inicio",
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="home" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="RidesStack"
+                component={RideStack}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: "Corridas",
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="car" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="FriendsStack"
+                component={FriendsStack}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: "Amigos",
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="users" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    headerShown: false,
+                    tabBarLabel: "Perfil",
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon name="user-circle" color={color} size={size} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
 };
--- src/screens/private/Search/index.js
+++ src/screens/private/Search/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { VStack, Text, ZStack } from "native-base";
 import {
-  requestForegroundPermissionsAsync,
-  getCurrentPositionAsync,
-  watchPositionAsync,
-  LocationAccuracy,
+    requestForegroundPermissionsAsync,
+    getCurrentPositionAsync,
+    watchPositionAsync,
+    LocationAccuracy,
 } from "expo-location";
 import MapView, { Marker } from "react-native-maps";
 
@@ -12,66 +12,62 @@ import Inputs from "./components/Inputs";
 import Party from "./components/Party";
 
 export default () => {
-  const [location, setLocation] = useState(undefined);
-  const [isLoading, setIsLoading] = useState(true);
+    const [location, setLocation] = useState(undefined);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    requestPermission();
-  }, []);
+    useEffect(() => {
+        requestPermission();
+    }, []);
 
-  useEffect(() => {
-    watchPositionAsync(
-      {
-        accuracy: LocationAccuracy.High,
-        timeInterval: 10000,
-        distanceInterval: 5,
-      },
-      (res) => {
-        //console.log("res callback ", res);
-        setLocation(res);
-      }
+    useEffect(() => {
+        watchPositionAsync(
+            {
+                accuracy: LocationAccuracy.High,
+                timeInterval: 10000,
+                distanceInterval: 5,
+            },
+            (res) => {
+                setLocation(res);
+            }
+        );
+    });
+
+    const requestPermission = async () => {
+        const options = await requestForegroundPermissionsAsync();
+
+        if (options.granted) {
+            const currentLocation = await getCurrentPositionAsync();
+            setLocation(currentLocation);
+            setIsLoading(false);
+        }
+    };
+
+    return (
+        <VStack flex={1} alignItems="center">
+            <Inputs top={0} />
+            {location == undefined ? null : (
+                <MapView
+                    style={{
+                        flex: 1,
+                        width: "100%",
+                    }}
+                    initialRegion={{
+                        latitude: location.coords.latitude,
+                        longitude: location.coords.longitude,
+                        latitudeDelta: 0.05,
+                        longitudeDelta: 0.05,
+                    }}
+                >
+                    <Marker
+                        coordinate={{
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+                        }}
+                    />
+                </MapView>
+            )}
+
+            <Party mx={2} position={"absolute"} bottom={4} />
+        </VStack>
     );
-  });
-
-  const requestPermission = async () => {
-    const options = await requestForegroundPermissionsAsync();
-
-    //console.log("options ", options);
-
-    if (options.granted) {
-      const currentLocation = await getCurrentPositionAsync();
-      //console.log("currentLocation ", currentLocation);
-      setLocation(currentLocation);
-      setIsLoading(false);
-    }
-  };
-
-  return (
-    <VStack flex={1} alignItems="center">
-      <Inputs position={"absolute"} top={0} />
-      {location == undefined ? null : (
-        <MapView
-          style={{
-            flex: 1,
-            width: "100%",
-          }}
-          initialRegion={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.05,
-            longitudeDelta: 0.05,
-          }}
-        >
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-            }}
-          />
-        </MapView>
-      )}
-
-      <Party mx={2} position={"absolute"} bottom={4} />
-    </VStack>
-  );
 };
